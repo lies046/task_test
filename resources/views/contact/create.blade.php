c18b2655cff8d329a8304136d7ba58ecf3af71a3
lies046 <skytree.skysky@gmail.com>
2020-07-14 19:54:20 +0900

    Modified CreateViews

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -15,6 +15,44 @@
                     @endif
 
                     createです
+                    <form method="POST" action="">
+                        氏名
+                        <input type="text" name="your_name">
+                        <br>
+                        件名
+                        <input type="text" name="title">
+                        <br>
+                        メールアドレス
+                        <input type="email" name="email">
+                        <br>
+                        ホームページ
+                        <input type="url" name="url">
+                        <br>
+                        性別
+                        <input type="radio" name="gender" value="0">男性
+                        <input type="radio" name="gender" value="1">女性
+                        <br>
+                        年齢
+                        <select name="age">
+                            <option value="">選択してください</option>
+                            <option value="1">~19歳</option>
+                            <option value="2">20歳~29歳</option>
+                            <option value="3">30歳~39歳</option>
+                            <option value="4">40歳~49歳</option>
+                            <option value="5">50歳~59歳</option>
+                            <option value="6">60歳~69歳</option> 
+                        </select>
+                        <br>
+                        お問い合わせ内容
+                        <textarea name="contact"></textarea>
+                        <br>
+
+                        <input class="form-check-input" type="checkbox" id="caution">
+                        <label class="form-check-label" for="caution">注意事項に同意する</label>
+                        <br>
+                        
+                        <input class="btn btn-info" type="submit" value="登録する">
+                    </form>
                 </div>
             </div>
         </div>
